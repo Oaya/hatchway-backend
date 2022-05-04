c8e9b80160b9f4ed16eb941f05bb405cc72b283a
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-03 22:21:35 -0700

    create sort for sortBy and direction

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,16 +3,30 @@ const router = require('express').Router();
 
 
 const postRoutes = () => {
-  router.get('/:tag', (req, res) => {
-    const tag = req.params.tag;
-    axios.get(`http://hatchways.io/api/assessment/blog/posts?tag=${tag}`).then((doc) => {
+  router.get('/:tags/:sortBy?/:direction?', (req, res) => {
+    const { tags, sortBy, direction } = req.params;
+    const sortByArr = ['id', 'reads', 'likes', 'popularity'];
+
+    //when tag is one string//
+    axios.get(`http://hatchways.io/api/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`).then((doc) => {
+
+      let post = doc.data.posts;
+
+      if (sortBy && sortByArr.includes(sortBy)) {
+        if (direction && direction.toLowerCase() === "desc") {
+          post = post.sort((a, b) => (b[sortBy] > a[sortBy] ? 1 : -1));
+        } else {
+          post = post.sort((a, b) => (b[sortBy] < a[sortBy] ? 1 : -1));
+        }
+      }
       res.status(200).send({
-        posts: doc.data.posts
+        posts: post
       })
+
     })
 
   })
-  return router
+  return router;
 }
 
 
