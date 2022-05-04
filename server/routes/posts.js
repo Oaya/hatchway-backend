001f7a43cdd13b5f04969496c24f064ebb8a3c7e
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-04 09:20:50 -0700

    error when the tags not working

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -13,37 +13,39 @@ const postRoutes = () => {
       (direction && !acceptableDirection.includes(direction))) {
       res.status(400).send({
         error: "sortBy parameter is invalid"
-      })
-    }
+      });
+    };
 
     //when tag is more than one string //
+    //fetch the post for every tag//
+    if (tags.includes(',')) {
 
+    } else {
+      //when tag is one string//
+      axios
+        .get(`http://hatchways.io/api/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`)
+        .then((doc) => {
+          let posts = doc.data.posts;
 
-    console.log(tags.includes(','))
+          if (direction && direction.toLowerCase() === "desc") {
+            posts = posts.sort((a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy]))
+          } else {
+            posts = posts.sort((a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]))
+          }
+          res.status(200).send({
+            posts: posts
+          });
 
-    //when tag is one string//
-    axios.get(`http://hatchways.io/api/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`).then((doc) => {
-
-      let posts = doc.data.posts;
-
-      if (direction && direction.toLowerCase() === "desc") {
-        // post = post.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
-        posts = posts.sort((a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy]))
-      } else {
-        // post = post.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
-        posts = posts.sort((a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]))
-      }
-
-      res.status(200).send({
-        posts: posts
-      })
-
-    })
-
-  })
+        })
+        .catch((err) => {
+          //error response when tags is not present//
+          res.status(400).send({
+            error: "Tags parameter is required"
+          });
+        });
+    }
+  });
   return router;
 }
-
-
 
 module.exports = postRoutes
