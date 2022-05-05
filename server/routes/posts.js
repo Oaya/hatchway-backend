627dce02cbc24f06cc94f4c8b609f456dde8f91e
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-04 17:19:53 -0700

    create function srotby direction

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,8 +4,9 @@ const router = require('express').Router();
 const postRoutes = () => {
   router.get('/:tags/:sortBy?/:direction?', (req, res) => {
     const { tags, sortBy, direction } = req.params;
-    const acceptableSort = ['id', 'reads', 'likes', 'popularity', undefined];
-    const acceptableDirection = ['desc', 'asc', undefined];
+    const acceptableSort = ['id', 'reads', 'likes', 'popularity'];
+    const acceptableDirection = ['desc', 'asc'];
+
 
     //error response when sortBy and direction are invalid//
     if ((sortBy && !acceptableSort.includes(sortBy)) ||
@@ -14,6 +15,16 @@ const postRoutes = () => {
         error: "sortBy parameter is invalid"
       });
     };
+
+    //sort array by direction//
+    const sortArray = (postsArray) => {
+      if (direction && direction.toLowerCase() === "desc") {
+        postsArray = postsArray.sort((a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy]))
+      } else {
+        postsArray = postsArray.sort((a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]))
+      }
+      return postsArray;
+    }
 
     //when tag is more than one string //
     if (tags.includes(',')) {
@@ -24,32 +35,30 @@ const postRoutes = () => {
         let url = axios.get(`http://hatchways.io/api/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`)
         allTagsURLs.push(url)
       });
-      // tagsArray.map(tag => {
-      //   allTagURLs.push(`http://hatchways.io/api/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`);
 
-      // })
-
-
-      // axios.all(allTagURLs.map((endpoint) => axios.get(endpoint))).then(
-      //   (data) => console.log(data),
-      // );
       //make the separate request for every tag specified//
       axios
         .all([...allTagsURLs])
-        .then(axios.spread((...resp) => {
-          let posts;
-          resp.map(response => {
-            console.log(response.data.posts)
-            posts = response.data.posts;
-
+        .then(axios.spread((...doc) => {
+          let posts = [];
+          doc.map(response => {
+            posts.push(...response.data.posts);
           })
+          //filter posts by id//
+          let postsArrUnique = [...new Map(posts.map(post => [post.id, post])).values()]
+
+          postsArrUnique = sortArray(postsArrUnique);
+
           res.status(200).send({
-            posts: posts
+            posts: postsArrUnique
           });
         }))
-
-
-
+        .catch((err) => {
+          //error response when tags is not present//
+          res.status(400).send({
+            error: "Tags parameter is required"
+          });
+        });
 
     } else {
       //when tag is one string//
@@ -57,12 +66,8 @@ const postRoutes = () => {
         .get(`http://hatchways.io/api/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`)
         .then((doc) => {
           let posts = doc.data.posts;
+          posts = sortArray(posts)
 
-          if (direction && direction.toLowerCase() === "desc") {
-            posts = posts.sort((a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy]))
-          } else {
-            posts = posts.sort((a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]))
-          }
           res.status(200).send({
             posts: posts
           });
