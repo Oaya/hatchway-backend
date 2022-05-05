dfb99ab3b87d817327dda778065ca6e3785b4b26
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-05 15:36:11 -0700

    test for check uniqueness for the response

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -5,6 +5,7 @@ import express from 'express';
 import { requireParams } from '../server/index';
 
 
+
 const router = express()
 router.use('/', app);
 
@@ -86,50 +87,56 @@ describe("/api/posts route", () => {
   // });
 
   describe('multiple tag params is passed with sortBy and direction', () => {
-    test('should response with a 200 status code', async () => {
-      const res = await request(router).get('/api/posts/tech,history/likes/desc');
-      expect(res.statusCode).toBe(200);
-      expect(res.headers['content-type']).toContain('application/json');
-    });
 
     test('each post should includes at least one of the tag', async () => {
-      const res = await request(router).get('/api/posts/tech,history/likes/desc');
+      const res = await request(router).get('/api/posts/tech,history');
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toContain('application/json');
+
       const posts = JSON.parse(res.text).posts;
       posts.map(post => {
         expect(post.tags.join("").includes('tech') ||
           post.tags.join("").includes('history'))
           .toBeTruthy()
       });
-    })
+    });
 
-    //   test('should not get posts that tag includes other tags', async () => {
-    //     const res = await request(router).get('/api/posts/tech');
-    //     const posts = JSON.parse(res.text).posts;
-    //     posts.map((post => {
-    //       expect(post.tags.includes('tech')).toBeTruthy()
-    //     }))
-    //   });
-    // })
+    test('should post be unique', async () => {
+      const res = await request(router).get('/api/posts/tech,history/reads/desc');
+      const posts = JSON.parse(res.text).posts;
+      const id = posts.map(post => post.id)
+      expect(new Set(id).size !== id.length).toBeFalsy()
+    });
 
+    test('should order by givin sortBy with default asc order', async () => {
+      const res = await request(router).get('/api/posts/tech,history/reads');
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toContain('application/json');
 
-
-    // test('should response with a 400 status code when sortBy param is not valid', async () => {
-    //   const res = await request(router).get('/api/posts/tech/notvalid');
-    //   expect(res.statusCode).toBe(400);
-    //   expect(res.headers['content-type']).toContain('application/json');
-    //   expect(res.text).toEqual("{\"error\":\"sortBy parameter is invalid\"}");
-    // });
-
-    // test('should response with a 400 status code when direction param is not valid', async () => {
-    //   const res = await request(router).get('/api/posts/tech/id/notvalid');
-    //   expect(res.statusCode).toBe(400);
-    //   expect(res.headers['content-type']).toContain('application/json');
-    //   expect(res.text).toEqual("{\"error\":\"sortBy parameter is invalid\"}");
-    // });
-
+      const posts = JSON.parse(res.text).posts;
+      expect(posts[1].reads).toBeGreaterThan(posts[0].reads);
+      expect(posts[2].reads).toBeGreaterThan(posts[1].reads);
+    });
   })
 
 
 
+  // test('should response with a 400 status code when sortBy param is not valid', async () => {
+  //   const res = await request(router).get('/api/posts/tech/notvalid');
+  //   expect(res.statusCode).toBe(400);
+  //   expect(res.headers['content-type']).toContain('application/json');
+  //   expect(res.text).toEqual("{\"error\":\"sortBy parameter is invalid\"}");
+  // });
+
+  // test('should response with a 400 status code when direction param is not valid', async () => {
+  //   const res = await request(router).get('/api/posts/tech/id/notvalid');
+  //   expect(res.statusCode).toBe(400);
+  //   expect(res.headers['content-type']).toContain('application/json');
+  //   expect(res.text).toEqual("{\"error\":\"sortBy parameter is invalid\"}");
+  // });
 
 })
+
+
+
+
